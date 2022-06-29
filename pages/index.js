5e6a3a06955a30461922e817f5f18c1af9a6da6f
Théo Sigaud <sigaudtheo@gmail.com>
2022-06-29 16:37:07 +0200

    Add card home

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,33 @@
 import { registerUser } from "../firebase/firebase";
 
 export default {
-  name: 'IndexPage', 
+  name: 'IndexPage',
+
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+      },
+
+      login: {
+        email: '',
+        password: '',
+      },
+    }
+  },
 
   methods: {
     async handleRegister(e) {
       console.log('mmmm');
       e.preventDefault();
-  
+
       const email = e.target.querySelector('input[type="email"]').value;
       const password = e.target.querySelector('input[type="password"]').value;
-  
+
       const user = await registerUser(email, password);
-  
+
       this.$router.push("/")
     }
   }
-} 
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,101 @@
 <template>
   <div>
-    <div>
-      <b-navbar toggleable="lg" type="dark" variant="info">
-        <b-navbar-brand href="#">Google Slides</b-navbar-brand>
-      </b-navbar>
-    </div>
-     <form v-on:submit.prevent="handleRegister">
-        <input type="email" />
-        <input type="password" />
-        <button type="submit">Register</button>
-      </form>
+    <header>
+      <div>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+          <b-navbar-brand href="#">Google Slides</b-navbar-brand>
+        </b-navbar>
+      </div>
+    </header>
+
+    <main>
+      <section class="d-flex justify-content-center mt-5">
+        <div>
+          <b-card title="Connexion" sub-title="">
+
+            <div>
+              <b-form @submit="handleRegister">
+                <b-form-group
+                  id="input-group-1"
+                  label="Email address:"
+                  label-for="input-1"
+                  description="We'll never share your email with anyone else."
+                >
+                  <b-form-input
+                    id="input-1"
+                    v-model="login.email"
+                    type="email"
+                    placeholder="Enter email"
+                    required
+                  ></b-form-input>
+                </b-form-group>
+
+                <b-form-group
+                  id="input-group-2"
+                  label="Password:"
+                  label-for="input-2"
+                  description="We'll never share your password with anyone else."
+                >
+                  <b-form-input
+                    id="input-2"
+                    v-model="login.password"
+                    type="password"
+                    placeholder="*****"
+                    required
+                  ></b-form-input>
+                </b-form-group>
+
+
+                <b-button type="submit" variant="primary">Connexion</b-button>
+              </b-form>
+            </div>
+          </b-card>
+        </div>
+
+
+        <div class="ml-5">
+          <b-card title="Inscription" sub-title="">
+
+            <div>
+              <b-form @submit="handleRegister">
+                <b-form-group
+                  id="input-group-1"
+                  label="Email address:"
+                  label-for="input-1"
+                  description="We'll never share your email with anyone else."
+                >
+                  <b-form-input
+                    id="input-1"
+                    v-model="form.email"
+                    type="email"
+                    placeholder="Enter email"
+                    required
+                  ></b-form-input>
+                </b-form-group>
+
+                <b-form-group
+                  id="input-group-2"
+                  label="Password:"
+                  label-for="input-2"
+                  description="We'll never share your password with anyone else."
+                >
+                  <b-form-input
+                    id="input-2"
+                    v-model="form.password"
+                    type="password"
+                    placeholder="*****"
+                    required
+                  ></b-form-input>
+                </b-form-group>
+
+
+                <b-button type="submit" variant="primary">Inscription</b-button>
+              </b-form>
+            </div>
+          </b-card>
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 
