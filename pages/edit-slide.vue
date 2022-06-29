088c6ca28c718846322b66f060c31f4e822d2695
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2022-06-29 17:07:28 +0200

    CkEditor - Started

---
+++ pages/edit-slide.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <CkEditor />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import CKEditor from '@ckeditor/ckeditor5-vue2';
+
+Vue.use( CKEditor );
+
+export default {
+  name: "edit-slide"
+}
+</script>
+
+<style scoped>
+
+</style>
