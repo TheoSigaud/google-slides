4306dbec2d1e59190f544d9b40db2ffcba1740d3
waseem1104 <waseem11@hotmail.fr>
2022-06-29 16:26:41 +0200

    Init slides - Add modal to create new document

---
+++ pages/slides/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand href="#">Google Slides</b-navbar-brand>
+
+            <b-navbar-nav class="ml-auto">
+                <b-button class="btn-slide" type="submit" v-b-modal.modal-1 >Nouveau document</b-button>
+            </b-navbar-nav>
+        </b-navbar>
+        <b-modal ok-only id="modal-1" title="Nouvelle présentation">
+            <div>
+                <b-form>
+                    <b-form-group id="input-group-1" label="Nom du document :">
+                        <b-form-input id="input-1" type="text" required></b-form-input>
+                    </b-form-group>
+                    <b-button type="submit" class="btn-slide">Créer</b-button>
+                </b-form>
+            </div>
+        </b-modal>
+    </div>
+</template>
+
+
