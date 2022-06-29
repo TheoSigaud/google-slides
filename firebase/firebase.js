feef204028c5d9514bb78172246a5aaf16ed367e
Samy Hamed-e-saberi <samy.saberi@gmail.com>
2022-06-29 16:16:35 +0200

    firease login

---
+++ firebase/firebase.js
@@ -0,0 +1,43 @@
+import { initializeApp } from "firebase/app";
+import {
+  getDatabase,
+  set,
+  ref,
+  push,
+  serverTimestamp,
+  onValue,
+  onChildAdded,
+} from "firebase/database";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  setPersistence,
+  browserLocalPersistence,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+import firebaseConfig from "./firebase.json";
+
+const app = initializeApp(firebaseConfig);
+
+const database = getDatabase(app);
+const auth = getAuth(app);
+
+export function getUser() {
+  return auth.currentUser;
+}
+
+export function getAuthState(cb = () => {}) {
+  onAuthStateChanged(auth, (user) => {
+    if (user) return cb(user);
+    cb(false);
+  });
+}
+
+export function registerUser(email, password) {
+  return setPersistence(auth, browserLocalPersistence).then(() => {
+    return createUserWithEmailAndPassword(auth, email, password).then(
+      (user) => user
+    );
+  });
+}
