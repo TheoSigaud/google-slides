bfd2680d95fae7e7343bf87af7163095265818c9
Théo Sigaud <sigaudtheo@gmail.com>
2022-07-01 14:55:45 +0200

    Fix modal

--- pages/slides/index.vue
+++ pages/slides/index.vue
@@ -7,13 +7,14 @@
                 <b-button class="btn-slide" type="submit" v-b-modal.modal-1 >Nouveau document</b-button>
             </b-navbar-nav>
         </b-navbar>
-        <b-modal ok-only id="modal-1" title="Nouvelle présentation">
+        <b-modal ok-only id="modal-1" title="Nouvelle présentation" hide-footer>
             <div>
                 <b-form @submit="onSubmit">
                     <b-form-group id="input-group-1" label="Nom du document :">
                         <b-form-input  v-model="form.name" id="input-1" type="text" required></b-form-input>
                     </b-form-group>
-                    <b-button type="submit" class="btn-slide">Créer</b-button>
+
+                    <b-button type="submit" class="btn-slide" @click="$bvModal.hide('modal-1')">Créer</b-button>
                 </b-form>
             </div>
         </b-modal>
